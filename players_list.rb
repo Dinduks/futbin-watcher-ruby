7c929a6f2583831525614a50970e10c6baf9db9b
Samy Dindane <samy@dindane.com>
2015-01-14 03:06:58 +0100

    Externalize players list

---
+++ players_list.rb
@@ -0,0 +1,12 @@
+PLAYERS_LIST = {
+    '/player/3072/cristiano-ronaldo' => 6000000,
+    '/player/3071/lionel-messi' => 4000000,
+    '/player/3074/arjen-robben' => 800000,
+    '/player/3083/luka-modric' => 20000,
+    '/player/3076/iniesta' => 20000,
+    '/player/3088/philipp-lahm' => 20000,
+    '/player/3209/jordi-alba' => 8000,
+    '/player/3086/sergio-ramos' => 170000,
+    '/player/3104/j%C3%A9r%C3%B4me-boateng' => 10000,
+    '/player/21155/manuel-neuer' => 350000,
+}
